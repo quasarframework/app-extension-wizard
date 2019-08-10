6032cf3a60e5f1260aed4f15c295f1b322424c4d
Daniel Thompson-Yvetot <denjell@quasar.dev>
2019-08-10 18:05:53 +0200

    feat(upgrade) refactor to listen to STDOUT and process.exit

--- src/index.js
+++ src/index.js
@@ -1,28 +1,43 @@
 const execa = require('execa')
 
+/**
+ *
+ * @param api
+ * @param force
+ * @returns {Promise<void>}
+ */
+const upgrade = async (api, force = '') => {
+  const notifyUpgrade = execa(
+    'quasar', ['upgrade', force],
+    {
+      cwd: api.resolve.app('.')
+    }
+  )
+  let { stdout } = await notifyUpgrade
+  return stdout
+}
+
+let stdout
+
 module.exports = function (api) {
-  api.beforeDev((api, { quasarConf }) => {
+  api.beforeDev(async (api, { quasarConf }) => {
     switch (api.prompts.upgrade) {
       case 'noUpgrade':
         break
       case 'notifyUpgrade':
-        execa.commandSync(
-          'quasar upgrade',
-          {
-            stdio: 'inherit',
-            cwd: api.resolve.app('.')
-          }
-        )
+        stdout = upgrade(api)
         setTimeout(function () {}, 2000)
         break
       case 'forceUpgrade':
-        execa.commandSync(
-          'quasar upgrade -i',
-          {
-            stdio: 'inherit',
-            cwd: api.resolve.app('.')
-          }
-        )
+        stdout = await upgrade(api, '-i')
+        console.log(stdout)
+        if(!stdout.includes('Congrats!')) {
+          console.log(`
+ Quasar Dependencies Have Been Updated by the ✨ WIZARD ✨.
+ Please rerun last command to make sure you are using the latest dependencies.
+           `)
+          process.exit(0)
+        }
         break
       default: // maybe they modified the extension data
         break
